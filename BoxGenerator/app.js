2accd167cbcac563ccb7c6cc321d51c007fbe3b8
root <root@LAPTOP-ODE2T0U9.localdomain>
2020-07-02 18:00:13 +0530

    Vinay(S-55)

--- BoxGenerator/app.js
+++ BoxGenerator/app.js
@@ -3,6 +3,7 @@ a.addEventListener('click', myFunction);
 
 var old = 0;
 var i = 1;
+
 function myFunction() {
     let n = document.getElementById('number').value;
     n = parseInt(old) + parseInt(n);
@@ -12,6 +13,7 @@ function myFunction() {
         document.getElementById('box').appendChild(b);
         b.innerHTML += i;
         old = document.getElementById('box').lastElementChild.innerHTML;
+
     }
 
 }
